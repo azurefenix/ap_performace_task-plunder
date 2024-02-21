c2f79b7bbe34d6a651db040689a676eeeabb8d79
L Dahm <90226889+azurefenix@users.noreply.github.com>
2024-02-21 15:51:28 -0600

    set player and different maps on join; function newLoc] add if statements; make sprite holders; change colors

--- main.ts
+++ main.ts
@@ -1,56 +1,66 @@
-function newLoc (num: number, list: any[]) {
-    posHolder = sprites.create(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `, SpriteKind.Player)
+controller.player4.onEvent(ControllerEvent.Connected, function () {
+    tiles.setCurrentTilemap(tilemap`fourPMap`)
+    p4 = sprites.create(assets.image`p4`, SpriteKind.Player)
+    controller.player4.moveSprite(p4)
+})
+// add bool so check if cross or storm
+// if cross can't be on land if storm can
+// if storm can't be on player if cross can
+function newLoc (list: tiles.Location[], cORs: boolean) {
+    posHolder = sprites.create(assets.image`storm`, SpriteKind.Player)
     while (true) {
         col = randint(0, 10)
         row = randint(0, 10)
         tiles.placeOnTile(posHolder, tiles.getTileLocation(col, row))
-        if (!(tiles.tileAtLocationEquals(tiles.getTileLocation(col, row), assets.tile`land`))) {
-        	
+        // cross true
+        // storm false
+        if (cORs) {
+            if (tiles.tileAtLocationEquals(posHolder.tilemapLocation(), assets.tile`sea`)) {
+                return posHolder.tilemapLocation()
+            }
+        } else {
+            if (posHolder.tilemapLocation() != p1.tilemapLocation()) {
+                if (posHolder.tilemapLocation() != p2.tilemapLocation()) {
+                    if (posHolder.tilemapLocation() != p3.tilemapLocation()) {
+                        if (posHolder.tilemapLocation() != p4.tilemapLocation()) {
+                            count = 0
+                            for (let value of list) {
+                                if (value == posHolder.tilemapLocation()) {
+                                    count += 1
+                                }
+                            }
+                            if (count == 0) {
+                                return posHolder.tilemapLocation()
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
+controller.player3.onEvent(ControllerEvent.Connected, function () {
+    tiles.setCurrentTilemap(tilemap`threePMap`)
+    p3 = sprites.create(assets.image`p3`, SpriteKind.Player)
+    controller.player3.moveSprite(p3)
+})
+controller.player2.onEvent(ControllerEvent.Connected, function () {
+    tiles.setCurrentTilemap(tilemap`twoPMap`)
+    p2 = sprites.create(assets.image`p2`, SpriteKind.Player)
+    controller.player2.moveSprite(p2)
+})
+controller.player1.onEvent(ControllerEvent.Connected, function () {
+    tiles.setCurrentTilemap(tilemap`onePMap`)
+    p1 = sprites.create(assets.image`p1`, SpriteKind.Player)
+    controller.player1.moveSprite(p1)
+})
+let count = 0
+let p3: Sprite = null
+let p2: Sprite = null
+let p1: Sprite = null
 let row = 0
 let col = 0
 let posHolder: Sprite = null
-let p1 = sprites.create(assets.image`p1`, SpriteKind.Player)
-controller.player1.moveSprite(p1)
-let p2 = sprites.create(assets.image`p2`, SpriteKind.Player)
-controller.player2.moveSprite(p2)
-let p3 = sprites.create(assets.image`p3`, SpriteKind.Player)
-controller.player3.moveSprite(p3)
-let p4 = sprites.create(img`
-    . . . . . . 7 7 7 7 7 . . . . . 
-    . . . . . . 7 7 7 7 7 . . . . . 
-    . . . . . . 7 7 7 7 7 . . . . . 
-    . . . . . . 7 . . . . . . . . . 
-    . . . . . . 7 . . . . . . . . . 
-    . . . . . . 7 . . . . . . . . . 
-    . . . . . . 7 . . . . . . . . . 
-    . . . . . . 7 . . . . . . . . . 
-    e e e e e e e e e e e e e e e e 
-    e e e e e e e e e e e e e e e e 
-    e e e e e e e e e e e e e e e e 
-    e e e e e e e e e e e e e e e e 
-    . e e e e e e e e e e e e e e . 
-    . . e e e e e e e e e e e e . . 
-    . . . e e e e e e e e e e . . . 
-    . . . . e e e e e e e e . . . . 
-    `, SpriteKind.Player)
-controller.player4.moveSprite(p4)
+let p4: Sprite = null
+let stormLocs: number[] = []
+let xLocs: number[] = []
