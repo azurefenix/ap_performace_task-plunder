0b80f2a4dc3ba8679d102245d505b8c3952b8f7f
L Dahm <90226889+azurefenix@users.noreply.github.com>
2024-02-27 16:11:39 -0600

    make plunder islands possible; start end game func; storm reset

--- main.ts
+++ main.ts
@@ -3,9 +3,6 @@ namespace SpriteKind {
     export const cross = SpriteKind.create()
     export const storm = SpriteKind.create()
 }
-/**
- * after certain amount of time, storm destroyed
- */
 controller.player4.onEvent(ControllerEvent.Connected, function () {
 	
 })
@@ -49,19 +46,83 @@ sprites.onDestroyed(SpriteKind.cross, function (sprite) {
         tiles.placeOnTile(cross, crossLocs.pop())
     }
 })
+function gameEnd () {
+    sprites.destroyAllSpritesOfKind(SpriteKind.Player)
+    sprites.destroyAllSpritesOfKind(SpriteKind.cross)
+    sprites.destroyAllSpritesOfKind(SpriteKind.storm)
+}
 controller.player3.onEvent(ControllerEvent.Connected, function () {
 	
 })
+info.player1.onScore(10, function () {
+    end = true
+    gameEnd()
+})
+function wantPlunderIsland () {
+    wantPlunder = game.askForString("Do you want to plunder this island? (y/n)", 1)
+    if (wantPlunder == "y") {
+        if (tryPlunderIsland()) {
+            game.splash("Island plundered")
+            info.player1.changeScoreBy(1)
+            pause(2000)
+        } else {
+            game.splash("Plunder unsuccessful")
+            pause(2000)
+        }
+    } else {
+        pause(2000)
+    }
+}
 // use counter variable for number of players, then direct to different functions for each version
 controller.player2.onEvent(ControllerEvent.Connected, function () {
 	
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.storm, function (sprite, otherSprite) {
-    sprite.setVelocity(5, 5)
+    info.player1.changeScoreBy(-1)
+    pause(2000)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.cross, function (sprite, otherSprite) {
-    sprites.destroy(otherSprite)
+    if (!(end)) {
+        sprites.destroy(otherSprite)
+        info.player1.changeScoreBy(1)
+    }
 })
+// easy: if guess number within 2 of random number
+// medium: if guess number within 1 of random number
+// hard: if guess number is random number
+function tryPlunderIsland () {
+    islandDifficulty = game.askForNumber("How many skulls were on the island? (1, 2, 3)", 1)
+    while (islandDifficulty > 3) {
+        islandDifficulty = game.askForNumber("How many skulls were on the island? (1, 2, 3)", 1)
+    }
+    islandStrength = randint(0, 10)
+    pStrength = game.askForNumber("Guess a number", 2)
+    if (islandDifficulty == 3) {
+        if (pStrength == islandStrength) {
+            return true
+        } else {
+            return false
+        }
+    } else if (islandDifficulty == 2) {
+        if (pStrength <= islandStrength + 1 && pStrength >= islandStrength - 1) {
+            return true
+        } else {
+            return false
+        }
+    } else if (islandDifficulty == 1) {
+        if (pStrength <= islandStrength + 2 && pStrength >= islandStrength - 2) {
+            return true
+        } else {
+            return false
+        }
+    } else {
+        return false
+    }
+}
+let pStrength = 0
+let islandStrength = 0
+let islandDifficulty = 0
+let wantPlunder = ""
 let count = 0
 let row = 0
 let col = 0
@@ -75,6 +136,8 @@ p1 = sprites.create(assets.image`p1`, SpriteKind.Player)
 controller.player1.moveSprite(p1)
 p1.setPosition(0, 0)
 scene.cameraFollowSprite(p1)
+info.player1.setScore(0)
+p1.setStayInScreen(false)
 end = false
 cross = sprites.create(assets.image`xTreasure`, SpriteKind.cross)
 crossLocs = [cross.tilemapLocation()]
@@ -84,9 +147,35 @@ let storm = sprites.create(assets.image`storm`, SpriteKind.storm)
 let stormLocs = [storm.tilemapLocation()]
 stormLocs.push(newLoc(stormLocs, false))
 tiles.placeOnTile(storm, stormLocs.pop())
-// plunder island
-// if guess number within 2 of random number, win island
-// island yours and get 1 pt
-if (p1.isHittingTile(CollisionDirection.Left)) {
-	
-}
+// after certain amount of time, storm destroyed
+forever(function () {
+    stormLocs.push(newLoc(stormLocs, false))
+    tiles.placeOnTile(storm, stormLocs.pop())
+    for (let index = 0; index < 6; index++) {
+        pause(2000)
+    }
+})
+forever(function () {
+    if (true) {
+        // ask if want to plunder island
+        // if y
+        // win island
+        // island yours and get 1 pt
+        // set island to plundered
+        // if plundered island plundered, island loose point
+        // if n
+        // wait 5 sec
+        if (p1.isHittingTile(CollisionDirection.Left)) {
+            wantPlunderIsland()
+        }
+        if (p1.isHittingTile(CollisionDirection.Top)) {
+            wantPlunderIsland()
+        }
+        if (p1.isHittingTile(CollisionDirection.Right)) {
+            wantPlunderIsland()
+        }
+        if (p1.isHittingTile(CollisionDirection.Bottom)) {
+            wantPlunderIsland()
+        }
+    }
+})
